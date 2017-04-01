e3be0a243d30b77f151b9990ea0fbda5c439f4ab
scaperoth <scaperoth@gmail.com>
2017-04-01 15:59:40 -0400

    final build

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+	
+	public GameObject hazard;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+	public GUIText scoreText;
+	public GUIText restartText;
+	public GUIText gameOverText;
+
+	private bool gameOver;
+	private bool restart;
+	private int score;
+
+	void Start ()
+	{
+		gameOver = false;
+		restart = false;
+		score = 0;
+
+		restartText.text = "";
+		gameOverText.text = "";
+
+		UpdateScoreText ();
+		StartCoroutine (SpawnWaves ());
+	}
+
+	void Update ()
+	{
+		if (restart) {
+			if (Input.GetKeyDown (KeyCode.R)) {
+				SceneManager.LoadScene ("Main");
+			}
+		}
+	}
+
+	IEnumerator SpawnWaves ()
+	{	
+		yield return new WaitForSeconds (startWait);
+
+		while (!gameOver) {
+			for (int i = 0; i < hazardCount; i++) {
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+
+				yield return new WaitForSeconds (spawnWait);
+
+			}
+			yield return new WaitForSeconds (waveWait);
+		}
+
+		restartText.text = "Press 'R' for Restart";
+		restart = true;
+	}
+
+	public void GameOver ()
+	{
+		gameOverText.text = "Game Over!";
+		gameOver = true;
+	}
+
+	public void AddScore (int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScoreText ();
+	}
+
+	void UpdateScoreText ()
+	{
+		scoreText.text = "Score: " + score.ToString ();
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,26 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class Boundary
-{
+public class Boundary{
 	public float xMin, xMax, zMin, zMax;
 }
 
 public class PlayerController : MonoBehaviour
 {
 
+	// ship motion and boundary 
 	public float speed;
 	public float zTilt, xTilt;
 	public Boundary boundary;
 
+	// shot variables 
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
+	// private
 	private Rigidbody rb;
+	private AudioSource boltSound;
+	private float nextFire;
 
 	// Use this for initialization
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody> ();
+		boltSound = GetComponent<AudioSource> ();
 	}
-	
+
+	void Update ()
+	{
+
+		// push button and time is ready? 
+		if ((Input.GetButton ("Fire1")) && Time.time > nextFire) {
+
+			// add to next fire the current time and 
+			// the time it takes for the next shop 
+			nextFire = Time.time + fireRate;
+
+			// lock the x rotation to keep the shots from 
+			// shooting up or down if the ship changes its orientation
+			shotSpawn.rotation = Quaternion.Euler (0.0f, 0.0f, shotSpawn.rotation.eulerAngles.z);
+
+			// drop a new shot onto the scene and it will fire itself
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+
+			boltSound.Play ();
+		}
+	}
+
 	// Update is called once per frame
 	void FixedUpdate ()
 	{
